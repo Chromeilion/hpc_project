f2cb65c94682c0ae48782a00685452eabb269d86
chromeilion <uki@cro.moe>
2024-03-28 18:18:37 +0100

    do work

--- plot_results.py
+++ plot_results.py
@@ -23,33 +23,38 @@ def main():
     avgs = calc_averages(res)
 
     for operation, op_res in avgs.items():
-        fig, ax = plt.subplots()
-        for alg, processes_res in op_res.items():
-            alg = alg_map[alg]
-
-            arrays = [np.array(i[1:]) for i in results]
-            avg_res = sum(arrays)/len(arrays)
-            ax.plot(avg_res[0, :], avg_res[1, :], label=alg)
-        col_titles = results[0][0]
-        ax.set_xlabel(col_titles[0])
-        ax.set_ylabel(col_titles[1])
-        ax.set_title(operation)
-        fig.legend()
-        fig.show()
+        for alg, alg_res in op_res.items():
+            for msg_size, msg_size_res in alg_res["msg_size"].items():
+                fig, ax = plt.subplots()
+                items = msg_size_res.items()
+                x = [int(i[0]) for i in items]
+                y = [i[1] for i in items]
+                ax.plot(x, y, label=alg)
+                ax.set_xlabel(alg_res['columns'][0])
+                ax.set_ylabel(alg_res['columns'][1])
+                ax.set_title(" ".join([operation, alg, str(msg_size)]))
+                fig.legend()
+                fig.show()
 
 
 def calc_averages(results: dict):
-    averages = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+    """
+    Convert to operation -> alg -> message_size -> num_procs
+    """
+    averages = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(dict))))
     for operation, alg_res in results.items():
         for alg, processes_res in alg_res.items():
             alg = alg_map[alg]
+            for no_procs, res in processes_res.items():
+                columns = res[0][0]
+                arrs = [np.array(i[1:]) for i in res]
+                avg_arr = sum(arrs) / len(arrs)
+                averages[operation][alg]["columns"] = columns
 
-            all_procs = processes_res.keys()
+                for i in avg_arr:
+                    size = np.round(i[0])
+                    averages[operation][alg]["msg_size"][size][no_procs] = i[1]
 
-            arrays = [np.array(i[1:]) for i in results]
-            avg_res = sum(arrays)/len(arrays)
-
-            averages[operation][alg].append((no_processes, avg_res[10, :]))
     return averages
 
 
